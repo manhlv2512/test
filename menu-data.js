b1c000d34f4d76d602973e19be36d36dc3b5ad75
Le Van Manh <manhlv2512@gmail.com>
2025-07-20 23:50:41 +0700

    2nd commit

--- menu-data.js
+++ menu-data.js
@@ -10,7 +10,7 @@ var menuItems = [
       },
       {
         "title": "ChatGPT",
-        "link": "https://chatgpt.com/",
+        "link": "https://chatgpt.com",
         "icon": "fas fa-globe"
       }
     ]
@@ -25,30 +25,15 @@ var menuItems = [
         "icon": "fas fa-link"
       },
       {
-        "title": "Install MongoDB",
-        "link": "https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/",
+        "title": "Mongoose",
+        "link": "https://mongoosejs.com/docs/5.x/docs/guide.html",
         "icon": "fas fa-link"
       },
       {
-        "title": "Install MongoDB",
-        "link": "https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/",
+        "title": "Stack Overflow",
+        "link": "https://stackoverflow.com/questions/72834345/how-to-add-array-index-field-to-items-in-mongodb-nested-array",
         "icon": "fas fa-link"
-      },
-      {
-        "title": "Install MongoDB",
-        "link": "https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/",
-        "icon": "fas fa-link"
-      },
-      {
-        "title": "Install MongoDB",
-        "link": "https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/",
-        "icon": "fas fa-link"
-      },
-      {
-        "title": "Install MongoDB",
-        "link": "https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-os-x/",
-        "icon": "fas fa-link"
-      },
+      }
     ]
   },
   {
@@ -61,20 +46,96 @@ var menuItems = [
         "icon": "fas fa-fire"
       },
       {
-        "title": "Tin thể thao",
-        "link": "https://www.24h.com.vn/bong-da-c48.html",
+        "title": "Điểm nóng",
+        "link": "https://www.24h.com.vn/diem-nong-c704.html",
         "icon": "fas fa-fire"
       },
       {
-        "title": "Giải trí",
-        "icon": "fas fa-film",
-        "link": "https://www.24h.com.vn/giai-tri-c731.html",
+        "title": "Dự báo thời tiết",
+        "link": "https://www.24h.com.vn/du-bao-thoi-tiet-c568.html",
+        "icon": "fas fa-sun"
       },
+      {
+        "title": "Lịch thi đấu bóng đá",
+        "link": "https://www.24h.com.vn/lich-thi-dau-bong-da-c287.html",
+        "icon": "fas fa-futbol"
+      },
+      {
+        "title": "Bóng đá Ngoại hạng Anh",
+        "link": "https://www.24h.com.vn/bong-da-ngoai-hang-anh-c149.html",
+        "icon": "fas fa-futbol"
+      },
+      {
+        "title": "Phim",
+        "link": "https://www.24h.com.vn/phim-c74.html",
+        "icon": "fas fa-film"
+      },
+      {
+        "title": "Giá vàng",
+        "icon": "fas fa-coins",
+        "link": "https://www.24h.com.vn/gia-vang-hom-nay-c425.html",
+      },
+      {
+        "title": "Ngắm xe đẹp",
+        "link": "https://www.24h.com.vn/anh-nguoi-dep-va-xe-c199.html",
+        "icon": "fas fa-car",
+      },
+      {
+        "title": "Bảng giá xe ô tô",
+        "link": "https://www.24h.com.vn/bang-gia-xe-o-to-c807.html",
+        "icon": "fas fa-car"
+      },
+      {
+        "title": "So sánh xe",
+        "link": "https://www.24h.com.vn/so-sanh-xe-c805.html",
+        "icon": "fas fa-car"
+      },
+      {
+        "title": "Tin tức công nghệ",
+        "link": "https://www.24h.com.vn/tin-tuc-cong-nghe-c453.html",
+        "icon": "fas fa-laptop"
+      },
+      {
+        "title": "Laptop giá rẻ",
+        "link": "https://www.24h.com.vn/laptop-gia-re-c451.html",
+        "icon": "fas fa-laptop"
+      }
     ]
   },
   {
     "title": "IBM Documentation",
     "icon": "fas fa-book",
     "link": "https://www.ibm.com/docs/en/bpm/8.6.0?topic=configuration-deploying-100customxml-file-deployment-environment",
+  },
+  {
+    "title": "GitHub",
+    "link": "https://github.com/",
+    "icon": "fab fa-github"
+  },
+  {
+    "title": "Stack Overflow",
+    "icon": "fab fa-stack-overflow",
+    "children": [
+      {
+        "title": "Tinder API Authentication Token Retrieval",
+        "link": "https://stackoverflow.com/questions/75411806/tinder-api-authentication-token-retrieval",
+        "icon": "fas fa-link"
+      },
+      {
+        "title": "How to Secure Nginx with Let’s Encrypt on Ubuntu 20.04",
+        "link": "https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-20-04",
+        "icon": "fas fa-link"
+      },
+      {
+        "title": "How to Install Nginx on Ubuntu 20.04",
+        "link": "https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-20-04",
+        "icon": "fas fa-link"
+      },
+      {
+        "title": "Encrypt/Decrypt JavaScript",
+        "link": "https://www.labnol.org/code/encrypt-decrypt-javascript-200307",
+        "icon": "fas fa-link"
+      }
+    ]
   }
 ];
