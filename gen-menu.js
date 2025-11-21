e9e47003c3960bc836e99b4fa71996408ca8a5ba
Le Van Manh <manhlv2512@gmail.com>
2025-11-21 19:11:05 +0700

    update

--- gen-menu.js
+++ gen-menu.js
@@ -177,31 +177,28 @@ function setAuth(username) {
   document.getElementById("username").textContent = username;
 }
 
-// Logout function
-function logout() {
-  // alert("Logging out...");
-  // Optional: clear tokens, sessionStorage, etc.
-  // sessionStorage.clear();
-  window.location.href = loginUrl; // redirect to login
-}
-
 function logout() {
   // Hiển thị hộp thoại xác nhận
-  var confirmed = confirm("Bạn có chắc chắn muốn đăng xuất không?");
-  if (!confirmed) return; // Nếu bấm 'Hủy' thì dừng lại
-
-  // Xóa dữ liệu người dùng trong sessionStorage và localStorage
-  sessionStorage.clear();
-  localStorage.clear();
-
-  // Xóa cookie (nếu có)
-  /* document.cookie.split(";").forEach(cookie => {
-    var name = cookie.split("=")[0].trim();
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-  }); */
-
-  // Chuyển hướng sang trang đăng nhập
-  window.location.href = loginUrl;
+  Swal.fire({
+    // title: "Bạn có chắc chắn muốn đăng xuất không?",
+    text: "Bạn có chắc chắn muốn đăng xuất không?",
+    // icon: "question",
+     icon: "warning",
+    // showCancelButton: true,
+    showCloseButton: true,
+    confirmButtonColor: "#863ACC",
+    // cancelButtonColor: "red",
+    confirmButtonText: "Đồng ý",
+    cancelButtonText: "Không đồng ý",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Xóa dữ liệu người dùng trong sessionStorage và localStorage
+      sessionStorage.clear();
+      localStorage.clear();
+      // Chuyển hướng sang trang đăng nhập
+      window.location.href = loginUrl;
+    }
+  });
 }
 
 // Toggle dropdown when clicking the user info area
@@ -233,7 +230,7 @@ document.addEventListener("click", function (e) {
 });
 
 // Thêm sự kiện hover cho menu-item khi sidebar đang đóng
-$(document).on("mouseenter", ".menu-item", function () {
+/* $(document).on("mouseenter", ".menu-item", function () {
   var sidebar = document.getElementById("sidebar");
   if (sidebar.classList.contains("collapsed")) {
     sidebar.classList.remove("collapsed");
@@ -247,7 +244,7 @@ $(document).on("mouseleave", ".menu-item", function () {
     sidebar.classList.remove("force-open");
     sidebar.classList.add("collapsed");
   }
-});
+}); */
 
 function escapeHtml(text) {
   return $("<div>").text(text).html();
